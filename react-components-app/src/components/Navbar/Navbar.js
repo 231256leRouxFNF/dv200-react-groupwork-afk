764ba040f77998afecd332123d5c0f16ef07f420
OpenWindow231312 <231312@virtualwindow.co.za>
2025-03-06 11:42:39 +0200

    fixed pages

--- react-components-app/src/components/Navbar/Navbar.js
+++ react-components-app/src/components/Navbar/Navbar.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="navbar">
-      <h1>Red Cars</h1>
+      <h1>Crimson Velocity</h1>
       <div className="dropdown">
         <button onClick={() => setIsOpen(!isOpen)} className="dropdown-toggle">
           Menu
         </button>
         {isOpen && (
           <div className="dropdown-menu">
-            <button onClick={() => console.log('Option 1 clicked')}>Mustang</button>
-            <button onClick={() => console.log('Option 2 clicked')}>Ferrari</button>
-            <button onClick={() => console.log('Option 3 clicked')}>Porsche</button>
+            <Link to="/" className="dropdown-item">
+              Home
+            </Link>
+            <Link to="/about" className="dropdown-item">
+              About
+            </Link>
+            <Link to="/contact" className="dropdown-item">
+              Contact
+            </Link>
           </div>
         )}
       </div>
